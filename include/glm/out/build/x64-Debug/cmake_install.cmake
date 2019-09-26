e269f4bcb6289567f67864d1425c0c2bedf54eb4
Owen King <Owen King@DESKTOP-204TB8O>
2019-09-25 23:51:49 -0400

    Added Assignment 2

--- include/glm/out/build/x64-Debug/cmake_install.cmake
+++ include/glm/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Owen King/Desktop/DSA 2/Git/include/glm/out/build/x64-Debug/CMakeFiles/CMakeTmp
+# Install script for directory: C:/Users/Owen King/Desktop/DSA 2/Git/include/glm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CMAKE_TRY_COMPILE")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Owen King/Desktop/DSA 2/Git/include/glm/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Owen King/Desktop/DSA 2/Git/include/glm/out/build/x64-Debug/CMakeFiles/CMakeTmp/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Owen King/Desktop/DSA 2/Git/include/glm/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
